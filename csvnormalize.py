9f5eac6d54a2ab4052caffe4cae478ddd0ffc599
Mark Jordan <mjordan@sfu.ca>
2013-10-09 13:28:17 -0700

    Catchup commit: added CSV derivative logic; added module admin form; cleaned up composite model XML; other fixes.

--- csvnormalize.py
+++ csvnormalize.py
@@ -2,40 +2,20 @@ import sys
 from subprocess import call
 import openpyxl
 import re
+import os.path
 
 # argv[2] is the process ID passed from inc/derivatives.inc.
 # We need this to avoid nameing conflicts on the csvlist file.
 csvlist_filepath = '/tmp/csvlist.' + sys.argv[2]
 
-# argv for filename here
-xlsx = re.search(r'\.xlsx', sys.argv[1])
-if xlsx is None:
-	xlsxname = re.sub(r'\.[A-Za-z]{3}$',r'\.xlsx', sys.argv[1])
-	# call(['ssconvert', sys.argv[1],xlsxname])
-	call(['ssconvert','--export-type=Gnumeric_Excel:xlsx',sys.argv[1],xlsxname])
-	workbookname = xlsxname
+# Development only
+if os.path.exists(csvlist_filepath):
+  o = open(csvlist_filepath, 'ab')
+  # o.write(sys.argv[1] + '\n')
+  o.write("/tmp/test_ds.csv\n")
 else:
-	workbookname = sys.argv[1]
-workbook = openpyxl.load_workbook(workbookname)
-sheets = get_sheet_names(workbook)
-totalsheets = len(sheets)
-o = open(csvlist_filepath, 'wb')
-if totalsheets!=1:
-	workbooks = {}
-	# for y in [1:totalsheets]:
-	for y in [totalsheets]:
-		workbook[y] = workbook
-    	# for x in [1:totalsheets]:
-    	for x in [totalsheets]:
-        	if x != y:
-            		workbook[y].remove_sheet(x)
-        cleanname = re.sub('[^a-zA-Z0-9\n\.]','',sheets[y+1])
-        filename = cleanname+'.xlsx'
-        workbook[y].save(filename)
-        csvname = cleanname+'.csv'
-        call(['in2csv',filename,'>',csvname])
-        o.write(csvname+'\n')
-else:
-	csvname = re.sub(r'\.xlsx?',r'\.csv', sys.argv[1])
-	call(['in2csv',workbookname,'>',csvname])
-	o.write(csvname)
+  o = open(csvlist_filepath, 'wb')
+  o.write("# Temporary data file. Do not edit.\n")
+  # o.write(sys.argv[1] + '\n')
+  o.write("/tmp/test_ds.csv\n")
+
