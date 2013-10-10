90b7258c83e983276b017e3e2a635ff4cdb4385b
Mark Jordan <mjordan@sfu.ca>
2013-10-09 22:06:47 -0700

    Roughed out logic for adding derivative CSV files from spreadsheets.

--- csvnormalize.py
+++ csvnormalize.py
@@ -1,21 +1,18 @@
+# Development placeholder script.
+
 import sys
-from subprocess import call
-import openpyxl
-import re
 import os.path
 
-# argv[2] is the process ID passed from inc/derivatives.inc.
-# We need this to avoid nameing conflicts on the csvlist file.
 csvlist_filepath = '/tmp/csvlist.' + sys.argv[2]
 
-# Development only
 if os.path.exists(csvlist_filepath):
   o = open(csvlist_filepath, 'ab')
-  # o.write(sys.argv[1] + '\n')
-  o.write("/tmp/test_ds.csv\n")
+  # o.write("/tmp/test_ds.csv\n")
+  o.write(sys.argv[1] + "\n")
 else:
   o = open(csvlist_filepath, 'wb')
-  o.write("# Temporary data file. Do not edit.\n")
-  # o.write(sys.argv[1] + '\n')
-  o.write("/tmp/test_ds.csv\n")
+  # o.write("/tmp/test_ds.csv\n")
+  o.write(sys.argv[1] + "\n")
+
+o.close()
 
