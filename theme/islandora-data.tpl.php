d09e377c0cbdcf9fae219fdc464058b67b196bfa
axfelix <axfelix@gmail.com>
2014-09-19 15:36:50 -0700

    added compatibility with solr metadata

--- theme/islandora-data.tpl.php
+++ theme/islandora-data.tpl.php
@@ -33,22 +33,6 @@
       </div>
     <?php endif; ?>
   </div>
+  <?php print $metadata; ?>
   </div>
-  <fieldset class="collapsible collapsed islandora-basic-image-metadata">
-  <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
-    <div class="fieldset-wrapper">
-      <dl class="islandora-inline-metadata islandora-basic-image-fields">
-        <?php $row_field = 0; ?>
-        <?php foreach($dc_array as $key => $value): ?>
-          <dt class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
-            <?php print $value['label']; ?>
-          </dt>
-          <dd class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
-            <?php print $value['value']; ?>
-          </dd>
-          <?php $row_field++; ?>
-        <?php endforeach; ?>
-      </dl>
-    </div>
-  </fieldset>
 </div>
